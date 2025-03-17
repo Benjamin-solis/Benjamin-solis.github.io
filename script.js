c5addd2619a9d7030677d555ef1ef93fa795cc3d
Benjamin Solis Duran <benjaminsolis@Benjamins-MacBook-Air-2.local>
2025-03-17 11:01:30 -0300

    018

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-// Al cargar la página, aseguramos que el mensaje esté oculto
 window.onload = function() {
     document.getElementById('mensajeConfirmacion').style.display = 'none';
 };
 
-// Función que muestra el mensaje de confirmación
 function mostrarMensaje() {
-    // Mostrar el mensaje de confirmación
     document.getElementById('mensajeConfirmacion').style.display = 'block';
 }
-
