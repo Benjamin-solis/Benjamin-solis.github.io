1ff445974f83f531a4714366a9092fc50e0f9da0
Benjamin Solis Duran <benjaminsolis@Benjamins-MacBook-Air-2.local>
2025-03-17 10:49:03 -0300

    016

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 
+
+window.onload = function() {
+    document.getElementById('mensajeConfirmacion').style.display = 'none';
+};
+
+
 function mostrarMensaje() {
   
     document.getElementById('mensajeConfirmacion').style.display = 'block';
