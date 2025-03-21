4be8ceb5b2f73fbb8ab31d959bd271231b8ace95
Benjamin Solis Duran <benjaminsolis@Benjamins-MacBook-Air-2.local>
2025-03-21 12:07:22 -0300

    043

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ window.onload = function() {
 function mostrarMensaje() {
     document.getElementById('mensajeConfirmacion').style.display = 'block';
 }
+
+let key = 'ff619b19a0584f80af05230b56271ee0';
+let url = `GET https://newsapi.org/v2/top-headlines?country=ch&apiKey=ff619b19a0584f80af05230b56271ee0
+`;
